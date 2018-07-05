8adda42700a3209b739b9e41731499477a221d7d
Michael Morris <mdam2345@gmail.com>
2018-07-05 00:38:37 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,7 +3,12 @@ class EmailParser
   def initialize(csv_emails)
       @csv_emails = csv_emails
     end
-
+    def parse 
+      csv_emails.split.collect do |address|
+        address.split(',') 
+      end.flatten.uniq 
+    end 
+  end 
 end
 
 
